a2ebb87082dd11a4083ef2c5837f07c3f502b9c0
YagoPazos <yagopb@gmail.com>
2024-09-17 14:44:21 +0200

    styling boat cards

--- frontend/src/components/Booking.jsx
+++ frontend/src/components/Booking.jsx
@@ -7,25 +7,25 @@ function Booking({ boats }) {
       {boats.length > 0 ? (
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
           {boats.map((boat, index) => (
-            <li key={index} className="flex">
-              <div className="card bg-base-100 shadow-xl w-full max-w-sm mx-auto">
-                <figure className="relative w-full h-48 overflow-hidden">
+            <li key={index} className="flex flex-col">
+              <div className="input-borders complex-shadow card bg-base-100 w-full max-w-sm mx-auto flex flex-col h-full">
+                <figure className="relative w-full h-72 overflow-hidden">
                   <img
                     src={boat.imagen}
                     alt="Boat image"
-                    className="absolute inset-0 w-full h-full object-cover object-center"
+                    className="absolute inset-0 w-full h-full object-cover object-bottom"
                   />
                 </figure>
-                <div className="card-body">
-                  <h2 className="card-title text-lg font-semibold">
+                <div className="card-body flex flex-col flex-grow">
+                  <h2 className="pl-2 card-title text-lg font-semibold">
                     {boat.type}
-                    <p className="flex justify-end text-sm">{boat.plazas}</p>
+                    <p className="pr-2 flex justify-end text-sm">{boat.plazas}</p>
                   </h2>
-                  <p className="text-sm">{boat.description}</p>
-                  <div className="card-actions justify-end mt-4">
-                    <div className="badge badge-outline">{boat.potencia} HP</div>
-                    <div className="badge badge-outline">{boat.size} M</div>
-                    <div className="badge badge-outline">
+                  <p className="text-sm flex-grow">{boat.description}</p>
+                  <div className="card-actions gap-2 justify-end mt-4">
+                    <div className="input-borders p-1 px-2">{boat.potencia} HP</div>
+                    <div className="input-borders p-1 px-2">{boat.size} M</div>
+                    <div className="input-borders p-1 px-2">
                       {boat.price} €/Day
                     </div>
                   </div>
