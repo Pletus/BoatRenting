507b9ecc5bc798441141335db75a14af0788d12f
YagoPazos <yagopb@gmail.com>
2024-10-29 17:36:29 +0100

    styled oneboat component

--- frontend/src/components/OneBoat.jsx
+++ frontend/src/components/OneBoat.jsx
@@ -5,7 +5,6 @@ import user from "../assets/user-solid.svg";
 import mail from "../assets/envelope.svg";
 import phone from "../assets/phone.svg";
 import paragraph from "../assets/paragraph.svg";
-import Form from "./Form";
 
 function OneBoat({ dateRange, selectedLocation }) {
   const { id } = useParams();
@@ -45,7 +44,7 @@ function OneBoat({ dateRange, selectedLocation }) {
       if (result.success) {
         Swal.fire({
           title: "Sucess!",
-          text: "Message submitted successfully!",
+          text: "I have received an email with all your travel information. We will get back to you soon with the confirmation! Thank you!",
           icon: "success",
         });
         form.reset();
@@ -110,111 +109,156 @@ function OneBoat({ dateRange, selectedLocation }) {
   };
 
   return (
-    <div className="hero bg-base-200 min-h-screen flex flex-col p-4">
-      <div className="hero-content md:gap-24 flex-col lg:flex-row">
+    <div className="hero bg-base-200 min-h-screen flex flex-col">
+      <div className="hero-content md:gap-24 flex flex-col lg:flex-row items-start py-12">
         <img
           src={boat[0].imagen}
           className="md:max-w-lg input-borders complex-shadow"
         />
-        <div className="p-12">
-          <h1 className="text-5xl font-bold mt-2 ml-2">{boat[0].type}</h1>
-          <p className="py-6">{boat[0].description}</p>
-          <p className="text-2xl font-bold mt-2">{selectedLocation}</p>
-          <p className="text-xl font-bold mt-2">
-            from {dateRange.start} till {dateRange.end}
+        <div className="p-6 bg-gray-100 rounded-lg shadow-lg">
+          <p className="mb-6 pl-2 text-lg text-gray-700">
+            {boat[0].description}
           </p>
+          <div className="bg-white rounded-lg p-6 shadow-md mb-6 transition-transform transform hover:-translate-y-2 hover:shadow-xl">
+            <h3 className="text-xl font-semibold text-blue-600 mb-4">
+              Boat Details
+            </h3>
+            <div className="flex flex-col">
+              <p className="text-gray-800">
+                <strong>Horsepower:</strong> {boat[0].potencia}
+              </p>
+              <p className="text-gray-800">
+                <strong>Meters of Length:</strong> {boat[0].size}
+              </p>
+              <p className="text-gray-800">
+                <strong>Maximum Number of Passengers:</strong> {boat[0].plazas}
+              </p>
+            </div>
+          </div>
+          <div className="bg-white rounded-lg p-6 shadow-md mb-6 transition-transform transform hover:-translate-y-2 hover:shadow-xl">
+            <h3 className="text-xl font-semibold text-blue-600 mb-4">
+              Location Details
+            </h3>
+            <p className="text-2xl font-bold text-gray-800">
+              Location:{" "}
+              <span className="text-blue-600">{selectedLocation}</span>
+            </p>
+            <p className="text-xl font-bold text-gray-800">
+              From: <span className="text-blue-600">{dateRange.start}</span>{" "}
+              till <span className="text-blue-600">{dateRange.end}</span>
+            </p>
+          </div>
           <button
-            className="btn btn-primary ml-2 mt-4 complex-shadow"
+            className="btn btn-primary mt-4 bg-blue-500 text-white font-semibold py-2 px-6 rounded-md hover:bg-blue-600 transition-colors shadow-md"
             onClick={() => setShowForm(true)}
           >
-            Rent this boat
+            Rent this Boat
           </button>
         </div>
       </div>
       {showForm && (
-        <form onSubmit={onSubmit} className="mx-6">
-          <h2 className="pb-2 text-blue-500 font-bold drop-shadow-xl">
-            Contact Form
+        <form
+          onSubmit={onSubmit}
+          className="p-6 flex flex-wrap items-center justify-center gap-6  bg-gray-100 shadow-lg rounded-md"
+        >
+          <h2 className="text-2xl w-full text-center text-blue-500 font-bold drop-shadow-xl mb-4">
+            Payment
           </h2>
-          <div className="input-box">
-            <label htmlFor="name">Full Name *</label>
-            <div className="input-group">
+          <p className="text-xl w-full text-center text-blue-500 font-semibold drop-shadow-xl mb-4">
+            Enter your data and we will send you an email with the confirmation
+            of your trip.
+          </p>
+          <div className="flex items-center gap-2">
+            <label htmlFor="name" className="font-medium whitespace-nowrap">
+              Full Name *
+            </label>
+            <div className="input-group flex items-center gap-2">
               <input
                 type="text"
                 name="name"
-                className="field"
-                placeholder=""
+                className="field p-2 border rounded w-40"
+                placeholder="Your Full Name"
                 required
               />
-              <img src={user} alt="" className="w-8 h-8 pt-4" />
+              <img src={user} alt="user icon" className="w-6 h-6" />
             </div>
           </div>
-          <div className="input-box flex flex-cols-2 gap-4">
-            <div className="w-2/3 ">
-              <label className="flex" htmlFor="email">
-                Email Address *
-              </label>
-              <div className="input-group">
-                <input
-                  type="email"
-                  name="email"
-                  className="field flex"
-                  placeholder=""
-                  required
-                />
-                <img src={mail} alt="" className="w-8 h-8 pt-4" />
-              </div>
-            </div>
-            <div className="w-1/3">
-              <label htmlFor="phone">Phone</label>
-              <div className="input-group">
-                <input
-                  type="tel"
-                  name=""
-                  className="field "
-                  placeholder=""
-                  required
-                />
-                <img src={phone} alt="" className="w-8 h-8 pt-4" />
-              </div>
+          <div className="flex items-center gap-2">
+            <label htmlFor="email" className="font-medium whitespace-nowrap">
+              Email Address *
+            </label>
+            <div className="input-group flex items-center gap-2">
+              <input
+                type="email"
+                name="email"
+                className="field p-2 border rounded w-40"
+                placeholder="you@example.com"
+                required
+              />
+              <img src={mail} alt="mail icon" className="w-6 h-6" />
             </div>
           </div>
-          <h1 className=" flex justify-start mt-6 font-f p-0 text-blue-700">
-            Contact preference
-          </h1>
-          <div className="flex gap-10">
-            <div>
+          <div className="flex items-center gap-2">
+            <label
+              htmlFor="card-number"
+              className="font-medium whitespace-nowrap"
+            >
+              Card Number *
+            </label>
+            <div className="input-group flex items-center gap-2">
+              <input
+                type="tel"
+                name="card-number"
+                className="field p-2 border rounded w-40"
+                placeholder="1234 5678 9012 3456"
+                required
+              />
+              <img src={phone} alt="phone icon" className="w-6 h-6" />
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-4">
+            <h3 className="text-lg font-semibold text-blue-700 whitespace-nowrap">
+              Contact Preference
+            </h3>
+            <label className="flex items-center gap-2">
               <input
                 className="custom-checkbox"
                 name="democheckbox"
                 type="checkbox"
                 value="email"
               />
-              <span className="font-f check-text"> Reply by email </span>
-            </div>
-            <div>
+              <span>Reply by email</span>
+            </label>
+            <label className="flex items-center gap-2">
               <input
                 className="custom-checkbox"
                 name="democheckbox"
                 type="checkbox"
-                value="Phone"
+                value="phone"
               />
-              <span className=" font-f check-text"> Reply by phone </span>
-            </div>
+              <span>Reply by phone</span>
+            </label>
           </div>
-          <div className="input-box">
-            <label htmlFor="message">Your Message *</label>
-            <div className="input-group">
+          <div className="flex items-center gap-2">
+            <label htmlFor="message" className="font-medium whitespace-nowrap">
+              Message
+            </label>
+            <div className="input-group flex items-center gap-2">
               <textarea
                 name="message"
-                className="field message font-f"
-                placeholder=""
+                className="field p-2 border rounded w-60"
+                placeholder="optional"
                 required
               ></textarea>
-              <img src={paragraph} alt="" className="w-8 h-8 pt-4" />
+              <img src={paragraph} alt="paragraph icon" className="w-6 h-6" />
             </div>
           </div>
-          <button type="submit">Submit</button>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white font-semibold py-2 px-6 rounded-md hover:bg-blue-600 transition-colors"
+          >
+            Submit
+          </button>
         </form>
       )}
     </div>
